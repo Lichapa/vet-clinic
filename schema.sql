e2e1ac5b26b43375de14c2d0530c1b444d84a5b0
Lichapa <lichapau@gmail.com>
2022-01-29 16:16:16 +0200

    added table owners added table species

--- schema.sql
+++ schema.sql
@@ -11,3 +11,18 @@ CREATE TABLE animals (
 );
 
 ALTER TABLE animalS ADD COLUMN species VARCHAR(40);
+
+/* Owners Table */
+CREATE TABLE owners(
+   id INT GENERATED ALWAYS AS IDENTITY,
+   full_name VARCHAR(60),
+   age INT,
+   PRIMARY KEY(id)
+);
+
+/* Species Table */
+CREATE TABLE species (
+    id INT GENERATED ALWAYS AS IDENTITY,
+     name VARCHAR(40),
+     PRIMARY KEY(id)
+     );
