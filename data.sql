169da663e1f12032f357f7913018ff8e7c9c147a
Lichapa <lichapau@gmail.com>
2022-01-29 17:40:09 +0200

    Inserted Owners Data

--- data.sql
+++ data.sql
@@ -11,3 +11,12 @@ INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered, weight_k
 INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES(1008, 'Angemon', '2005-06-12', 1, TRUE, -45);
 INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES(1009, 'Boarmon', '2005-06-07', 7, TRUE, 20.4);
 INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES(1010, 'Blossom', '1998-10-13', 3, TRUE, 17);
+
+
+/* Insert the following data into the owners table: */
+INSERT INTO owners(full_name, age) VALUES('Sam Smith', 34);
+INSERT INTO owners(full_name, age) VALUES('Jennifer Orwell', 19);
+INSERT INTO owners(full_name, age) VALUES('Bob', 45);
+INSERT INTO owners(full_name, age) VALUES('Melody Pond', 77);
+INSERT INTO owners(full_name, age) VALUES('Dean Winchester', 14);
+INSERT INTO owners(full_name, age) VALUES('Jodie Whittaker', 38);
