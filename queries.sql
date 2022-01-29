529b25f9a549c1b26db7f6a9debe3e7d1c7bd297
Lichapa <lichapau@gmail.com>
2022-01-29 11:22:23 +0200

    Inserted data into the table Animals

--- queries.sql
+++ queries.sql
@@ -9,7 +9,9 @@ SELECT * FROM animals WHERE neutered = TRUE;
 SELECT * FROM animals WHERE name != 'Gabumon';
 SELECT * FROM animals WHERE weight_kg = 10.4 AND weight_kg = 17.3;
 
-INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES(1005, 'Charmander', '2020-02-08', 0, FALSE, 11);
-INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES(1006, 'Plantmon', '2022-11-15', 2, TRUE, 5.7);
-INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES(1007, 'Squirtle', '1993-04-02', 3, FALSE, 12.13);
-INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES(1008, 'Angemon', '2005-06-12', 1, TRUE, 45);
+INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES(1005, 'Charmander', '2020-02-08', 0, FALSE, -11);
+INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES(1006, 'Plantmon', '2022-11-15', 2, TRUE, -5.7);
+INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES(1007, 'Squirtle', '1993-04-02', 3, FALSE, -12.13);
+INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES(1008, 'Angemon', '2005-06-12', 1, TRUE, -45);
+INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES(1009, 'Boarmon', '2005-06-07', 7, TRUE, 20.4);
+INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES(1010, 'Blossom', '1998-10-13', 3, TRUE, 17);
