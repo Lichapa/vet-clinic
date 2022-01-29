79b729812f81c2bbdf2b5398ebe42e12286cd699
Lichapa <lichapau@gmail.com>
2022-01-29 09:47:36 +0200

    Created animals table Inserted data in Animals Table Wrote queries for

--- data.sql
+++ data.sql
@@ -1,5 +1,6 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO animals VALUES(1001, 'Agumon', '2020-02-03', 0, TRUE, 10.23);
+INSERT INTO animals VALUES(1002, 'Gabumon', '2018-11-15', 2, TRUE, 8);
+INSERT INTO animals VALUES(1003, 'Pikachu', '2018-01-07', 1, FALSE, 15.04);
+INSERT INTO animals VALUES(1004, 'Devimon', '2017-05-12', 5, TRUE, 11);
--- schema.sql
+++ schema.sql
@@ -1,5 +1,12 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    name varchar(100)
+    id INT,
+    name varchar(40),
+    date_of_birth DATE,
+    escape_attempts INT,
+    neutered BOOLEAN,
+    weight_kg DECIMAL(4,2),
+    PRIMARY KEY(id)
 );
+
